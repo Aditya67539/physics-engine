1816414296ec58d70eec2dba76ddb343ac359c04
Aditya Chaudhary <aditya67539@gmail.com>
2025-11-06 20:05:44 +0530

    feat: Add static bodies support

--- src/engine/Body.js
+++ src/engine/Body.js
@@ -1,13 +1,14 @@
 import Vec2 from "./Vec2.js";
 
 export class Body {
-    constructor(x, y, radius=25, vel_x=0, vel_y=0, mass=1.0) {
+    constructor(x, y, radius=25, vel_x=0, vel_y=0, mass=1.0, isStatic=false) {
         this.pos = new Vec2(x, y);
         this.prev_pos = new Vec2(x - vel_x * (1/60), y - vel_y * (1/60)); // for verlet integration
         this.acc = new Vec2(0, 0);
         this.radius = radius;
         this.circumference = this.radius * 2;
         this.mass = mass;
+        this.isStatic = isStatic;
     }
 
     getVelocity(dt) {
@@ -23,6 +24,8 @@ export class Body {
     }
 
     update(dt) {
+        if (this.isStatic) return;
+        
         let vel = this.pos.clone().sub(this.prev_pos);
         this.prev_pos.copy(this.pos);
         this.pos.add(vel).add(this.acc.clone().mult(dt ** 2));
