9acfcfca321556e2d4631144926d4cfd32beadf7
Aditya Chaudhary <aditya67539@gmail.com>
2025-11-06 19:56:58 +0530

    feat: Add boundary collision resolution

--- src/engine/World.js
+++ src/engine/World.js
@@ -1,5 +1,6 @@
 import Vec2 from "./Vec2.js";
 import { Body } from "./Body.js";
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from "./settings.js";
 
 export class World {
     constructor(gravity=new Vec2(0, 981.4)) {
@@ -18,6 +19,27 @@ export class World {
 
         for (let body of this.bodies) {
             body.update(dt);
+            this.boundaryCollision(body);
+        }
+    }
+
+    boundaryCollision(body) {
+        let vel = body.pos.clone().sub(body.prev_pos);
+
+        if (body.pos.x - body.radius < 0) {
+            body.pos.x = body.radius;
+            body.prev_pos.x = body.pos.x + vel.x;
+        } else if (body.pos.x + body.radius > SCREEN_WIDTH) {
+            body.pos.x = SCREEN_WIDTH - body.radius;
+            body.prev_pos.x = body.pos.x + vel.x;
+        }
+
+        if (body.pos.y - body.radius < 0) {
+            body.pos.y = body.radius;
+            body.prev_pos.y = body.pos.y + vel.y;
+        } else if (body.pos.y + body.radius > SCREEN_HEIGHT) {
+            body.pos.y = SCREEN_HEIGHT - body.radius;
+            body.prev_pos.y = body.pos.y + vel.y;
         }
     }
 }
